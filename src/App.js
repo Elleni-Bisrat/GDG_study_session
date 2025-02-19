e303ee0139ccaaf4e247d58ff3e209d8a82f1b31
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-02-19 22:43:56 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+
+    const counterhandeler = () => {
+    setCount((s) => s + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Props name="Elleni Bisrat" email="ellenibisrat94@gmail.com" age="21" />
+      <p className="count_btn">count : {count}</p>
+      <button onClick={counterhandeler} className="countChange">Count</button>
     </div>
   );
 }
+
+const Props = (props) => {
+  return (
+    <div className="container">
+      <p>
+        Name: <span style={{ color: "darkblue" }}>{props.name}</span>{" "}
+      </p>
+      <p>
+        Email: <span style={{ color: "yellow" }}>{props.email}</span>{" "}
+      </p>
+      <p>
+        Age: <span style={{ color: "purple" }}>{props.age}</span>
+      </p>
+        
+    </div>
+  );
+};
 
 export default App;
